aafbd29437ffd51a18d5a4633efae08678637f27
Nitin <Subedi>
2025-11-22 11:40:03 +0545

    added new project for even and odd checking

---
+++ evenorodd/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int num;
+	printf("number to check even or odd: ");
+	scanf("%d", &num);
+	if (num % 2 == 0)	{
+		printf("%d is even\n",num);
+	} else {
+		printf("%d is odd\n",num);
+	}
+}
+
