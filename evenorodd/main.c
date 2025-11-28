dd3116a0756bb618a2930bf3adfdee8a8d17a873
Nitin <Subedi>
2025-11-28 16:05:00 +0545

    commit

--- evenorodd/main.c
+++ evenorodd/main.c
@@ -5,9 +5,9 @@ int main(){
 	printf("number to check even or odd: ");
 	scanf("%d", &num);
 	if (num % 2 == 0)	{
-		printf("%d is odd\n",num);
-	} else {
 		printf("%d is even\n",num);
+	} else {
+		printf("%d is odd\n",num);
 	}
 }
 
