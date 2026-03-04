bc4352f85297ab5d38553e20d186c5aa11e5a9fa
Nitin <Subedi>
2026-03-04 20:24:34 +0545

    new

--- sum-of-two-numbers/main.c
+++ sum-of-two-numbers/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-int main(){
-	int first,second,sum;
-	printf("Sum calcuation by: Nitin\n");
-	printf("Enter the two numbers: ");
-	scanf("%d %d", &first, &second);
-	sum = first + second;
-	printf("The sum of these two numbers is: %d\n", sum);
+int main() {
+  int marks;
+  printf("enter your marks in computer: \r");
+  scanf("%d", &marks);
+	if (marks > 30){
+		printf("you have passed \n");
+	}else {
+		printf("you have failed\n");
+	}
 }
