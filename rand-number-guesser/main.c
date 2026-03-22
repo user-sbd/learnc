790cce0dc9597635955eff43b20886e26dbd2636
Nitin <Subedi>
2026-03-22 19:45:50 +0545

    [adding] random number guesser (interactive)

---
+++ rand-number-guesser/main.c
@@ -0,0 +1,28 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+  srand(time(NULL));
+  int guess;
+  int attempts = 0;
+  int num = rand() % 100;
+  bool guessed = false;
+
+  while (!guessed) {
+    printf("Enter the guess: ");
+    scanf("%d", &guess);
+    attempts++; 
+    if(guess>num) {
+      printf("The number is lower\n");
+    }
+    else if (guess<num){
+      printf("The number is higher\n");
+    }
+    else {
+      printf("You are right!\n You completed in %d attempts\n", attempts);
+      guessed = true;
+    }
+
+} }
